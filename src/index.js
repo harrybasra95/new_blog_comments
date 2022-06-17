8fc76af3011759875b9ea72dba891771e7dce4a4
Hanarinder Singh <harnarinder.singh@yum.com>
2022-06-17 13:01:19 +0530

    Added request parsing config for incoming request

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import express from 'express'
 
 const app = express()
 
+// Parser config for POST requests
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.listen(3000, () => {
     console.log('listening on port 3000')
 })
